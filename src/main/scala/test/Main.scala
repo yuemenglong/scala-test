5744b833bf494633a2f7d5a36fd3e8edbc5b419e
yuemenglong <yuemenglong@126.com>
2018-05-08 10:15:35 +0800

    add: 什么都不干，申请一个AppId然后立刻释放

--- src/main/scala/test/Main.scala
+++ src/main/scala/test/Main.scala
@@ -1,7 +1,13 @@
 package test
 
+import java.nio.ByteBuffer
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("hello world")
+    val buffer = ByteBuffer.allocate(1000)
+    buffer.putInt(100)
+    buffer.putDouble(10)
+    println(buffer.position())
   }
 }
--- src/main/scala/yarn/Main.scala
+++ src/main/scala/yarn/Main.scala
@@ -1,6 +1,30 @@
 package yarn
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.yarn.api.records.FinalApplicationStatus
+import org.apache.hadoop.yarn.client.api.{AMRMClient, NMClient}
+import org.apache.hadoop.yarn.conf.YarnConfiguration
+
 object Main {
   def main(args: Array[String]): Unit = {
+    // Initialize clients to ResourceManager and NodeManagers
+    val conf = new YarnConfiguration
+
+    val rmClient = AMRMClient.createAMRMClient
+    rmClient.init(conf)
+    rmClient.start()
+
+    val nmClient = NMClient.createNMClient
+    nmClient.init(conf)
+    nmClient.start()
+
+    // Register with ResourceManager
+    System.out.println("before registerApplicationMaster")
+    rmClient.registerApplicationMaster("", 0, "")
+    System.out.println("after registerApplicationMaster")
+
+    System.out.println("before unregisterApplicationMaster")
+    rmClient.unregisterApplicationMaster(FinalApplicationStatus.SUCCEEDED, "", "")
+    System.out.println("after unregisterApplicationMaster")
   }
 }
