bacf862083ec28c78d484c6a2ade52c86d7bacc9
yuemenglong <yuemenglong@126.com>
2018-05-14 08:11:55 +0800

    add: sync

--- src/main/scala/pack/jvm/common/UnreachableException.scala
+++
@@ -1,8 +0,0 @@
-package pack.jvm.common
-
-/**
-  * Created by <yuemenglong@126.com> on 2018/2/24.
-  */
-class UnreachableException extends RuntimeException("Unreachable") {
-
-}
--- src/main/scala/yarn/Main.scala
+++ src/main/scala/yarn/Main.scala
@@ -1,5 +1,9 @@
 package yarn
 
+import java.net.InetSocketAddress
+import java.util.Map.Entry
+
+import org.apache.commons.lang3.reflect.FieldUtils
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.yarn.api.records.{FinalApplicationStatus, Priority, Resource}
 import org.apache.hadoop.yarn.client.api.AMRMClient.ContainerRequest
@@ -7,6 +11,10 @@ import org.apache.hadoop.yarn.client.api.{AMRMClient, NMClient}
 import org.apache.hadoop.yarn.conf.YarnConfiguration
 import org.apache.hadoop.yarn.util.Records
 import org.apache.hadoop.yarn.util.resource.Resources
+import org.apache.log4j.Level
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.Logger
+import org.apache.log4j.PatternLayout
 
 /*
 yarn jar \
@@ -18,12 +26,30 @@ yarn jar \
 
 object Main {
   def main(args: Array[String]): Unit = {
+
+    val rootLogger = Logger.getRootLogger
+    rootLogger.setLevel(Level.INFO)
+    rootLogger.addAppender(new ConsoleAppender(new PatternLayout("%-6r [%p] %c - %m%n")))
     // Initialize clients to ResourceManager and NodeManagers
     val conf = new YarnConfiguration
+
+    //    conf.getSocketAddr(YarnConfiguration.RM_SCHEDULER_ADDRESS, YarnConfiguration.DEFAULT_RM_SCHEDULER_ADDRESS, YarnConfiguration.DEFAULT_RM_SCHEDULER_PORT)
+    conf.setSocketAddr(YarnConfiguration.RM_SCHEDULER_ADDRESS, new InetSocketAddress("211.159.173.48", YarnConfiguration.DEFAULT_RM_SCHEDULER_PORT))
 
     val rmClient: AMRMClient[ContainerRequest] = AMRMClient.createAMRMClient()
     rmClient.init(conf)
     rmClient.start()
+
+    //    println("=========================================")
+    //    println(rmClient.getClass)
+    //    println(FieldUtils.readField(rmClient, "rmClient", true).getClass)
+    //    println(conf)
+    //    val iter = conf.iterator()
+    //    while (iter.hasNext) {
+    //      val entry = iter.next
+    //      println(entry.getKey, entry.getValue)
+    //    }
+    //    println("=========================================")
 
     val nmClient = NMClient.createNMClient
     nmClient.init(conf)
