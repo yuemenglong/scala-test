62b5ca06968c112571e804b8c0268a91d2cc932c
yuemenglong <yuemenglong@126.com>
2018-05-21 08:02:59 +0800

    remove: scala dependency

--- src/main/scala/test/Main.scala
+++ src/main/scala/test/Main.scala
@@ -1,21 +1,20 @@
 package test
 
-import java.lang
+import java.{lang, util}
 import java.nio.ByteBuffer
 
 import com.carrotsearch.sizeof.RamUsageEstimator
 
 import scala.collection.mutable.ArrayBuffer
+import java.net.URI
+import java.nio.file._
+import java.io.{File, FileInputStream, FileOutputStream, InputStream}
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val arr = new ArrayBuffer[Object]()
-    (1 to 1000000).foreach(i => {
-      val s = new s1mme_bearer
-      s.init()
-      arr += s
-    })
-    Thread.sleep(1000000000)
-    println(arr.length)
+
   }
 }
